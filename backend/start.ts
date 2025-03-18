bf79a23060e062f9f736ee196dcc5ca0ff2d32e0
Nikhil Raikwar <raikwarnikhil80@gmail.com>
2025-03-18 23:13:28 +0530

    change the files

--- backend/start.ts
+++
@@ -1,9 +0,0 @@
-import { ReadableStream } from "stream/web";
-
-// Apply ReadableStream polyfill globally
-if (!globalThis.ReadableStream) {
-  (globalThis as any).ReadableStream = ReadableStream;
-}
-
-// Import and start the server
-import "./server.js";
--- dist/backend/start.js
+++
@@ -1,8 +0,0 @@
-import { ReadableStream } from "stream/web";
-// Apply ReadableStream polyfill globally
-if (!globalThis.ReadableStream) {
-    globalThis.ReadableStream = ReadableStream;
-}
-// Import and start the server
-import "./server.js";
-//# sourceMappingURL=start.js.map
